18b5545bd890e1794b5b6c5d48881d58a060a0f8
Muhammad Ubaid <ubaid1tahir@gmail.com>
2024-08-01 04:00:31 +0500

    Update About US

--- components/About.js
+++ components/About.js
@@ -12,7 +12,8 @@ function About() {
                             <div className="text-center my-5">
                                 <h2 className="display-5 fw-bolder"><span className="text-gradient d-inline">About Me</span></h2>
                                 <p className="lead fw-light mb-4">My name is Muhammad Ubaid and I am a MERN Stack Developer.</p>
-                                <p className="text-muted">MERN Stack Developer with 2 years of expertise in crafting robust web solutions. Proficient in MongoDB, Express.js, React.js, and Node.js. Passionate about creating efficient and scalable applications. Committed to delivering high-quality code for optimal user experiences. Let&apos;s build the future of the web together.</p>
+                                <p className="lead fw-light mb-4">My email is mu4991472@gmail.com</p>
+                                <p className="text-muted">MERN Stack Developer with 3 years of expertise in crafting robust web solutions. Proficient in MongoDB, Express.js, React.js, and Node.js. Passionate about creating efficient and scalable applications. Committed to delivering high-quality code for optimal user experiences. Let&apos;s build the future of the web together.</p>
                                 <div className="d-flex justify-content-center fs-2 gap-4">
                                     <a className="text-gradient" href="#!"><i className="bi bi-twitter"></i></a>
                                     <a className="text-gradient" href="#!"><i className="bi bi-linkedin"></i></a>
